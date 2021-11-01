091926174bdcdd8ae60c576cf895799848334229
Beksultannn <62333272+Beksultannn@users.noreply.github.com>
2021-11-01 16:04:03 +0600

    Add files via upload

---
+++ frontend/scripts/script.js
@@ -0,0 +1,13 @@
+var x = 50;
+var y = "yo";
+for (var i = 0; i < x; i++){
+    y = y + i + " ";
+} 
+
+// Тут через класс
+here = document.getElementsByClassName("here") 
+here [1].innerHTML = "i mam"
+
+// Тут через айди
+first_here = document.getElementById("here1")
+first_here.innerHTML = y
